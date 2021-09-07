04c28c875a808dcf54dbeefef912972f5237f2ae
Leonardo <leocv0205@gmail.com>
2021-09-07 16:03:11 -0500

    Se codifican las funciones matematicas para el rebote de la pelota

--- javaScript1/pong/main.js
+++ javaScript1/pong/main.js
@@ -28,6 +28,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle =0;
+        this.max_bounce_angle =Math.PI / 12;
+        this.speed = 3; 
 
         board.ball= this;
         this.kind = "circle";
@@ -39,9 +42,27 @@
         move: function(){
             this.x +=(this.speed_x * this.direction );
             this.y +=(this.speed_y);
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        collision: function(bar){
+            //reacción a la colisión
+            var relative_intersect_y =(bar.y +(bar.height / 2)) - this.y;
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed *Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction =1;
         }
     }
-
 })();
 
 (function(){
@@ -106,6 +127,7 @@
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
             
